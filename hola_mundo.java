f97f4ba027c72526d1b4e37b0746d9a70c2b7574
DiegoArmandoCH <91149406+DiegoArmandoCH@users.noreply.github.com>
2021-10-05 14:18:23 -0500

    holamundo en JAVA

---
+++ hola_mundo.java
@@ -0,0 +1,12 @@
+public class hola_mundo{
+	/**Titulo:Hola mundo en java 
+	 *Descripcion:Escribiendo codigo de java
+	 *Autor:Diego Armando Cruz Herández 
+	 *Fecha:20211004
+	 *Use:C:\Users\19403219\Documents\Java
+	 *Notas:Hola mundo en codigo Java
+	*/
+		public static void main(String []args){
+			System.out.println("HOLA MUNDO"); //prints Hello Word 
+	}
+}
