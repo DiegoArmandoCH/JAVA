d58d290228c66b0cde08e26747fc78cee4dd3aa7
DiegoArmandoCH <91149406+DiegoArmandoCH@users.noreply.github.com>
2021-10-05 14:47:35 -0500

    holamundo en java
    Código correcto de java

---
+++ holamundo.java
@@ -0,0 +1,15 @@
+public class holamundo{
+	/**
+	*
+	*Aplicacion java
+	*Title:		Hola mundo en JAVA
+	*Descrition:	hola mundo en JAVA desde vim en windows
+	*Author:	Diego Armando Cruz Herández 
+	*Date		20211005
+	*Version:	1
+	*Usage:		C:\Users\19403219\Documents\Java
+	*/
+		public static void main(String []args){
+			System.out.println("HOLA MUNDO");//prints Hello World
+	}
+}
